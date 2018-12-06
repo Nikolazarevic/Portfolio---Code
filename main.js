531b4ddd212dedb0a033b970327646362321b1b2
Nikola Lazarevic <nikola261292@gmail.com>
2018-12-06 02:20:47 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ $(document).ready(function() {
     $('#header').removeClass('shrink');
     }
   });
-	$('.html').animate({width: '95%'}, 4000);
-	$('.css').animate({width: '95%'}, 4000);
-	$('.jQuery').animate({width: '40%'}, 4000);
-	$('.JavaScript').animate({width: '30%'}, 4000);
-	$('.microsoft').animate({width: '95%'}, 4000);
-	$('.illustrator').animate({width: '65%'}, 4000);
-	$('.photoshop').animate({width: '90%'}, 4000);
-	$('.flash').animate({width: '40%'}, 4000);
-	$('.xd').animate({width: '90%'}, 4000);
-	$('.sass').animate({width: '95%'}, 4000);
-	$('.ajax').animate({width: '20%'}, 4000);
-	$('.git').animate({width: '20%'}, 4000);
+// 	$('.html').animate({width: '95%'}, 4000);
+// 	$('.css').animate({width: '95%'}, 4000);
+// 	$('.jQuery').animate({width: '40%'}, 4000);
+// 	$('.JavaScript').animate({width: '30%'}, 4000);
+// 	$('.microsoft').animate({width: '95%'}, 4000);
+// 	$('.illustrator').animate({width: '65%'}, 4000);
+// 	$('.photoshop').animate({width: '90%'}, 4000);
+// 	$('.flash').animate({width: '40%'}, 4000);
+// 	$('.xd').animate({width: '90%'}, 4000);
+// 	$('.sass').animate({width: '95%'}, 4000);
+// 	$('.ajax').animate({width: '20%'}, 4000);
+// 	$('.git').animate({width: '20%'}, 4000);
 });
