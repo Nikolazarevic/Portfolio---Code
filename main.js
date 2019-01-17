c8ce720294c70baf2cddc6a0401fb9165dfae774
Nikola Lazarevic <nikola261292@gmail.com>
2019-01-17 08:35:26 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,27 +21,33 @@ $(document).ready(function() {
 //     }
 //     });
 // }
-// 	$('.html').animate({width: '95%'}, 4000);
-// 	$('.css').animate({width: '95%'}, 4000);
-// 	$('.jQuery').animate({width: '40%'}, 4000);
-// 	$('.JavaScript').animate({width: '30%'}, 4000);
-// 	$('.microsoft').animate({width: '95%'}, 4000);
-// 	$('.illustrator').animate({width: '65%'}, 4000);
-// 	$('.photoshop').animate({width: '90%'}, 4000);
-// 	$('.flash').animate({width: '40%'}, 4000);
-// 	$('.xd').animate({width: '90%'}, 4000);
-// 	$('.sass').animate({width: '95%'}, 4000);
-// 	$('.ajax').animate({width: '20%'}, 4000);
-// 	$('.git').animate({width: '20%'}, 4000);
+	$('.html').animate({width: '95%'}, 4000);
+	$('.css').animate({width: '95%'}, 4000);
+	$('.jQuery').animate({width: '40%'}, 4000);
+	$('.JavaScript').animate({width: '30%'}, 4000);
+	$('.microsoft').animate({width: '95%'}, 4000);
+	$('.illustrator').animate({width: '65%'}, 4000);
+	$('.photoshop').animate({width: '90%'}, 4000);
+	$('.flash').animate({width: '40%'}, 4000);
+	$('.xd').animate({width: '90%'}, 4000);
+	$('.sass').animate({width: '95%'}, 4000);
+	$('.ajax').animate({width: '20%'}, 4000);
+	$('.git').animate({width: '20%'}, 4000);
 	clicked = true;
 	$('.fas').on('click', function() {
 		$('.navBar').slideToggle('slow');
 		// if(clicked) {
 		// 	$('.toggle').css('background-color', 'red');
 		// 	clicked = false;
-		// } else {
+		// } else {k
 		// 	$('.toggle').css('background-color', 'transparent');
 		// 	clicked = true;
 		// }
 	});
+	$( ".work" ).hover( function() {
+    $(this).find(".mask").css("opacity", "1");
+  	}, function() {
+    $(this).find(".mask").css("opacity", "0");
+  }
+);
 });
