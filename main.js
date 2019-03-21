764197f9a6db7a45dcc95e72840ffb8676cb610f
Nikola Lazarevic <nikola261292@gmail.com>
2019-03-21 18:03:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,98 @@
 $(document).ready(function() {
 	$(window).on('scroll', function() {
-  	if($(document).scrollTop() > 100) {
-    	$('#header').addClass('shrink');
-    }
-    else {
-    $('#header').removeClass('shrink');
-    }
+		var y = $(this).scrollTop();
+	  	if( y >= 100) {
+	    	$('#header').addClass('shrink');
+	    }
+	    else {
+	    	$('#header').removeClass('shrink');
+	    }
+		if ( y >= 1350 ) {
+			$('.htmlScale').animate({
+				width: '95%'
+			}, 1900);
+			$('.cssValue').fadeIn(3000);
+		};
+		if ( y >= 1400 ) {
+			$('.cssScale').animate({
+				width: '95%'
+			}, 1900);
+			$('.cssValue').fadeIn(3000);
+		};
+		if ( y >= 1450 ) {
+			$('.jqueryScale').animate({
+				width: '40%'
+			}, 1900);
+			$('.jqueryValue').fadeIn(3000);
+		};
+		if ( y >= 1500 ) {
+			$('.javascriptScale').animate({
+				width: '30%'
+			}, 1900);
+			$('.javascriptValue').fadeIn(3000);
+		};
+		if ( y >= 1600 ) {
+			$('.msScale').animate({
+				width: '95%'
+			}, 1900);
+			$('.msValue').fadeIn(3000);
+		};
+		if ( y >= 1700 ) {
+			$('.illustratorScale').animate({
+				width: '65%'
+			}, 1900);
+			$('.illustratorValue').fadeIn(3000);
+		};
+		if ( y >= 1800 ) {
+			$('.photoshopScale').animate({
+				width: '90%'
+			}, 1900);
+			$('.photoshopValue').fadeIn(3000);
+		};
+		if ( y >= 1850 ) {
+			$('.flashScale').animate({
+				width: '40%'
+			}, 1900);
+			$('.flashValue').fadeIn(3000);
+		};
+		if ( y >= 1900 ) {
+			$('.adobexdScale').animate({
+				width: '90%'
+			}, 1900);
+			$('.adobexdValue').fadeIn(3000);
+		};
+		if ( y >= 2000 ) {
+			$('.sassScale').animate({
+				width: '95%'
+			}, 1900);
+			$('.sassValue').fadeIn(3000);
+		};
+		if ( y >= 2050 ) {
+			$('.ajaxScale').animate({
+				width: '20%'
+			}, 1900);
+			$('.ajaxValue').fadeIn(3000);
+		};
+		if ( y >= 2100 ) {
+			$('.gitScale').animate({
+				width: '20%'
+			}, 1900);
+			$('.gitValue').fadeIn(3000);
+		// // };
+		// if ($('.htmlScale').css('margin-top','50%')){
+		// $('.htmlScale').animate({
+		// 	width: '95%'
+		// }, 1900);
+		// $('.htmlValue').fadeIn(3000);
+	};
   });
-// 	var fas = $('.fas');
-// 	klik = 0;
-// 	boja();
-// 	function boja() {
-// 	fas.on('click', function(){
-// 		klik++;
-//         $('.navBar').slideToggle('slow');
-//         $('.toggle').css('background-color','rgba(23, 28, 37, 0.9)');
-//     if (klik == 2) {
-//         $('.toggle').delay('500').css('background-color','transparent');
-//     	klik = 0;
-//     }
-//     });
-// }
-	$('.html').animate({width: '95%'}, 4000);
-	$('.css').animate({width: '95%'}, 4000);
-	$('.jQuery').animate({width: '40%'}, 4000);
-	$('.JavaScript').animate({width: '30%'}, 4000);
-	$('.microsoft').animate({width: '95%'}, 4000);
-	$('.illustrator').animate({width: '65%'}, 4000);
-	$('.photoshop').animate({width: '90%'}, 4000);
-	$('.flash').animate({width: '40%'}, 4000);
-	$('.xd').animate({width: '90%'}, 4000);
-	$('.sass').animate({width: '95%'}, 4000);
-	$('.ajax').animate({width: '20%'}, 4000);
-	$('.git').animate({width: '20%'}, 4000);
-	clicked = true;
+	
+
+
+
+
 	$('.fas').on('click', function() {
 		$('.navBar').slideToggle('slow');
-		// if(clicked) {
-		// 	$('.toggle').css('background-color', 'red');
-		// 	clicked = false;
-		// } else {k
-		// 	$('.toggle').css('background-color', 'transparent');
-		// 	clicked = true;
-		// }
 	});
 	$( ".work" ).hover( function() {
     $(this).find(".mask").css("opacity", "1");
